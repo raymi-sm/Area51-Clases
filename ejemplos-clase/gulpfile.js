cb3af6460d86fb59389b61baa404c223a0dc36b1
Raymi Saldomando <raymi15@gmail.com>
2017-02-19 17:50:53 -0500

    clase03 ejemplos y slides

--- ejemplos-clase/gulpfile.js
+++ ejemplos-clase/gulpfile.js
@@ -6,19 +6,19 @@ var gulp    = require('gulp'),
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "02",
+        server: "03",
         port: 1504
     });
 
-    gulp.watch("02/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("02/*.html").on('change', browserSync.reload);
-    gulp.watch("02/*.js").on('change', browserSync.reload);
+    gulp.watch("03/*.scss", ['sass']).on('change', browserSync.reload);
+    gulp.watch("03/*.html").on('change', browserSync.reload);
+    gulp.watch("03/*.js").on('change', browserSync.reload);
 });
 
 gulp.task("sass", function(){
-  return gulp.src('02/*.scss')
+  return gulp.src('03/*.scss')
   .pipe(sass({ outputStyle: 'compressed'}))
-  .pipe(gulp.dest('02/'));
+  .pipe(gulp.dest('03/'));
 });
 
 gulp.task('default', ['serve']);
