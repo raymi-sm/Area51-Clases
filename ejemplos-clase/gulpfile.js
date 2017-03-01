3f7607db54aa973a4bb156451a45c9ce1ef75536
Raymi Saldomando <raymi15@gmail.com>
2017-03-01 18:11:58 -0500

    clase07

--- ejemplos-clase/gulpfile.js
+++ ejemplos-clase/gulpfile.js
@@ -6,14 +6,14 @@ var gulp    = require('gulp'),
 gulp.task('serve', function() {
 
     browserSync.init({
-        server: "06",
+        server: "07/Solucion",
         port: 1504
     });
 
     //gulp.watch("05/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("06/css/*.css").on('change', browserSync.reload);
-    gulp.watch("06/*.html").on('change', browserSync.reload);
-    gulp.watch("06/*.js").on('change', browserSync.reload);
+    gulp.watch("07/Solucion/css/*.css").on('change', browserSync.reload);
+    gulp.watch("07/Solucion/*.html").on('change', browserSync.reload);
+    gulp.watch("07/Solucion/*.js").on('change', browserSync.reload);
 });
 
 /*gulp.task("sass", function(){
