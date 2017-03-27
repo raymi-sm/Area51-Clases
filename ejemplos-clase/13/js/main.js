8a1f265a6093a27b3a8a0c9795d8ff4ee04bf308
Raymi Saldomando <raymi15@gmail.com>
2017-03-27 07:37:45 -0500

    slides clase13

---
+++ ejemplos-clase/13/js/main.js
@@ -0,0 +1,9 @@
+// Ejemplo 1
+
+alert('Mi primer programa en JavaScript');
+document.write("<h1>Este es un título generado desde JavaScript</h1>");
+console.log("Este mensaje lo verás en la consola de Chrome");
+
+// Ejemplo variables
+
+var mensaje = "Mi primer programa en JavaScript";
--- ejemplos-clase/gulpfile.js
+++ ejemplos-clase/gulpfile.js
@@ -6,14 +6,14 @@
 gulp.task('serve', function() {
 
     browserSync.init({
-        server: "12",
+        server: "13",
         port: 1504
     });
 
     //gulp.watch("05/*.scss", ['sass']).on('change', browserSync.reload);
-    gulp.watch("12/css/*.css").on('change', browserSync.reload);
-    gulp.watch("12/*.html").on('change', browserSync.reload);
-    gulp.watch("12/*.js").on('change', browserSync.reload);
+    gulp.watch("13/css/*.css").on('change', browserSync.reload);
+    gulp.watch("13/*.html").on('change', browserSync.reload);
+    gulp.watch("13/*/*.js").on('change', browserSync.reload);
 });
 
 /*gulp.task("sass", function(){
