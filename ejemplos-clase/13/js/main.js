2d52f4ba515d4352faca40d5fcb4d08e75021f21
alumno <alumno>
2017-03-30 07:16:48 -0500

    clase13

--- ejemplos-clase/13/js/main.js
+++ ejemplos-clase/13/js/main.js
@@ -2,11 +2,11 @@
 
 /*alert('Mi primer programa en JavaScript');
 document.write("<h1>She's hungry</h1>");
-console.log("Este mensaje lo verás en la consola de Chrome");*/
+console.log("Este mensaje lo verás en la consola de Chrome");
 var nombres = prompt("Por favor ingresa tu nombre");
 alert("Gracias por ingresar tu nombre");
 
-var sustantivo = prompt("Escriba aqui su sustantivo");
+var sustantivo = prompt("Escriba aqui su sustantivo");*/
 
 /*parseInt(nombres);
 console.log(nombres);
@@ -18,6 +18,26 @@ function caminar (){
 caminar();*/
 // Ejemplo variables
 
-debugger;
-var mensaje = "Hola, mi nombre es " +  nombres + "y me apellido " + apellido;
+
+/*var mensaje = "Hola, mi nombre es " +  nombres + "y me apellido " + apellido;
 document.write(mensaje);
+alert("Hola");
+document.write("Hola me voy a imprimir en la pantalla");*/
+
+// variable vacia
+
+/*var nombre = prompt("Escribe aqui tu nombre");
+document.write(nombre);
+
+var x = 10;
+var y = 6;
+var resultado = x + y;
+
+var jugador = 0;
+
+jugador =+ 20;*/
+
+var edad = prompt("Escribe aqui tu edad");
+
+var resultado = parseInt(edad) + 40;
+document.write(resultado);
