c7e14abb7b2fd0c0d486938174e5294fddc5bcab
Raymi Saldomando <raymi15@gmail.com>
2017-04-03 17:37:28 -0500

    clase12

---
+++ ejemplos-clase/modulo-3/Clase-01/Strings(Cadenas de Texto)/main.js
@@ -0,0 +1,24 @@
+var nombre=prompt("ingresa tu nombre");
+var apellido=prompt("ingresa tu apellido");
+//var nombre = "Raymi";
+//var apellido = "Saldomando";
+//console.log(nombre + " " + apellido);
+//console.log("La cantidad de caracteres ingresados en el nombre es : " + nombre.length )
+//console.log("La cantidad de caracteres ingresados en el apellido es : " + apellido.length )
+
+console.log("Mi nombre y apellido en mayusculas es " + nombre.toUpperCase() + " " + apellido.toUpperCase())
+console.log("Mi nombre y apellido en mayusculas es " + nombre.toLowerCase() + " " + apellido.toLowerCase())
+
+//console.log(nombre.split(" "));
+
+console.log(nombre.split("a"));
+
+//console.log(nombre.split(""));
+
+//console.log(nombre.slice(4));
+
+//console.log(nombre.charAt(0).toUpperCase()+nombre.slice(1));
+
+//var cadena = prompt("Ingrese una cadena rara");
+
+//console.log(nombre.charAt(cadena.length-1));
--- ejemplos-clase/modulo-3/Clase-01/ejemplo06 objeto/main.js
+++ ejemplos-clase/modulo-3/Clase-01/ejemplo06 objeto/main.js
@@ -1,25 +1,19 @@
-var persona=
-		{
-			nombre:"juan carlos",
-			edad:22,
-			correo:"jtorriccelli",
-			telefono:"11111111",
-			dni:"17869458"
+var persona = {
+	nombre:"juan carlos",
+	edad:22,
+	correo:"jtorriccelli",
+	telefono:"11111111",
+	dni:"17869458"};
 
-		};
-
-var encuesta=
-		{
-			fecha:"10/10/2014",
-			nombre:"Juan Carlos",
-			correo:"jtorriccelli",
-			telefono:"11111111",
-			dni:"17869458",
-			edad:22,
-			distrito:"lince"
-		};
-
-
+var encuesta = {
+	fecha:"10/10/2014",
+	nombre:"Juan Carlos",
+	correo:"jtorriccelli",
+	telefono:"11111111",
+	dni:"17869458",
+	edad:22,
+	distrito:"lince"
+};
 
 console.log(persona["nombre"]);
 console.log(persona["edad"]);
@@ -30,6 +24,3 @@ console.log(persona.edad);
 persona.nombre="nuevo nombre";
 
 persona.estadoCivil="soltero";
-
-
-
