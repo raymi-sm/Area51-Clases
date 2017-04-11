5d90ae887a3d9eb62bad859cff5baccf13c9848d
alumno <alumno>
2017-04-10 19:20:29 -0500

    clase12

--- ejemplos-clase/modulo-3/Clase-03/menu-scroll/Solucion/main.js
+++ ejemplos-clase/modulo-3/Clase-03/menu-scroll/Solucion/main.js
@@ -1,7 +1,6 @@
 /* 
   Flujo del ejercicio:
     
-    Cuando el usuario hace scroll, el menú activa aquel link que corresponde a la sección correspondiente al link.
     Cuando el usuario clickea en uno de los links, la ventana scrollea por si misma hasta la ubicación que corresponde a la sección del link
 
   Lista de Pasos:
@@ -11,7 +10,6 @@
   3. Cuando uno de los enlaces sea clickeado  la ventana se animará con una propiedad de scrolling desde jQuery, la cual será equivalente al valor en pixeles donde se encuentra la sección que pertenece al link
 
 */
-
 
 
 var nav = $('nav');
