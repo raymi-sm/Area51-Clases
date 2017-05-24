a91e49a33994179f40c5a8b3ef6b26a68fe3b1bd
Raymi Saldomando <raymi15@gmail.com>
2017-05-24 17:12:30 -0500

    clase22

--- Ejemplos/Modulo03/clase22/ejercicios-clase/jQuery/Anidando-Elementos/app.js
+++ Ejemplos/Modulo03/clase22/ejercicios-clase/jQuery/Anidando-Elementos/app.js
@@ -1,37 +1,10 @@
-var toggleList = $('#toggleList');
-var listDiv = $('.list');
-var descriptionInput = $('input.description');
-var descriptionP = $('p.description');
-var descriptionButton = $('button.description');
 var addItemInput = $('input.addItemInput');
-var addItemButton = $('button.addItemButton');
+var ul = $('ul');
 
-toggleList.on('click', function(){
-  if(listDiv.css('display') === 'none' ){
-    $(this).text('Hide list');
-    listDiv.css('display', 'block');
-  }else {
-    $(this).text('Show list');
-    listDiv.css('display', 'none');
-  }
-});
+ul.append('<li>' + addItemInput.val() + '</li>');
+ul.prepend('<li>' + addItemInput.val() + '</li>');
 
-descriptionButton.on('click', function(){
-    descriptionP.text(descriptionInput.val());
-    descriptionInput.val("");
-});
 
-addItemButton.on('click', function(){
-  var ul = $('ul');
-  var li = $('ul li');
-  ul.append('<li>' + addItemInput.val() + '</li>');
-  addItemInput.val("");
-});
-
-removeItemButton.on('click', function(){
-  var itemLast = $('li:last-child');
-  itemLast.remove();  
-});
   
   
   
