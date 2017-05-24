4c1aee796cda86fb02d36fd43b22c2d437236f2c
Raymi Saldomando <raymi15@gmail.com>
2017-05-24 16:52:00 -0500

    clase22

---
+++ Ejemplos/Modulo03/clase22/ejercicios-alumnos/js-app/Solucion/jquery/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  var addValue = $("#add");
+  addValue.on('click', function(e){
+    e.preventDefault();
+    var inputField = $('#post-field');
+    var inputFieldValue = inputField.val();
+    if(inputFieldValue) {
+      var lista = $("#to-do");
+      var item =  $("<li class='item'>" + inputFieldValue + "</li>");
+      lista.append(item);
+      inputField.val("");
+    }else {
+      console.log('no clickees el botón antes de colocar un valor en el campo de texto');
+    }
+
+    var listaItems = $(".item");
+    var titulo = $(".titulo-dinamico");
+    listaItems.each(function(){
+        $(this).on("click", function(){
+          var itemHTML = $(this).html();
+         titulo.html(itemHTML);
+        })
+    })
+  });
+});
