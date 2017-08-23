c614c3f0c825f5831e49ccd8cd1e19a66e2b94fb
Raymi Saldomando <raymi@Raymis-MacBook-Pro.local>
2017-08-23 18:19:47 -0500

    modificando ejemplos para  clases

--- Ejemplos/Modulo03/clase24/ejercicios-clase/Aplicacion-TODO/main.js
+++ Ejemplos/Modulo03/clase24/ejercicios-clase/Aplicacion-TODO/main.js
@@ -10,45 +10,42 @@
 	8. Obtener el valor del elemento y cambiar el titulo por defecto por dicho valor
 */
 
-(function(){
-    var addValue = document.querySelector("#add");
-    
-    addValue.addEventListener("click", function(event){
-        event.preventDefault();
-        var campoTexto = document.getElementById("post-field").value;
-        var titulo = document.querySelector(".titulo-dinamico");
-        if(campoTexto){
-            var lista = document.getElementById("to-do");
-            var item = document.createElement("li");
-            item.className = "item";
-            item.innerHTML = "<p>" + campoTexto + "</p>" + "<div><span class='fa fa-trash-o'></span>" + "<span class='fa fa-check'></span></div>";
-            lista.appendChild(item);
-            document.getElementById("post-field").value = "";
-        }else {
-            alert("OYEEE TIENES QUE PONER ALGO PRIMERO U_U");
-        }
+(function() {
+  var addValue = document.querySelector("#add");
 
-        var listaItems = Array.from(document.querySelectorAll(".item"));
-				var basurita = document.querySelector(".fa-trash-o");
-				/*
+  addValue.addEventListener("click", function(event) {
+    event.preventDefault();
+    var campoTexto = document.getElementById("post-field").value;
+    var titulo = document.querySelector(".titulo-dinamico");
+    if (campoTexto) {
+      var lista = document.getElementById("to-do");
+      var item = document.createElement("li");
+      item.className = "item";
+      //item.innerHTML = "<p>" + campoTexto + "</p>" + "<div><span class='fa fa-trash-o'></span>" + "<span class='fa fa-check'></span></div>";
+      item.innerHTML = "<p>" + campoTexto + "</p>";
+      lista.appendChild(item);
+      document.getElementById("post-field").value = "";
+    } else {
+      alert("OYEEE TIENES QUE PONER ALGO PRIMERO U_U");
+    }
+
+    var listaItems = Array.from(document.querySelectorAll(".item"));
+    var basurita = document.querySelector(".fa-trash-o");
+    /*
         for(var contador = 0; contador < listaItems.length; contador++){
             listaItems[contador].addEventListener("click", function(){
                 titulo.innerHTML = campoTexto;
             });
         }
-				*/
-				listaItems.forEach(function(item){
-					item.addEventListener("click", function(){
-						titulo.innerHTML = campoTexto;
-					});
-					basurita.addEventListener("click", function(){
-						item.style.display = 'none';
-						titulo.style.display = 'none';
-					})
-				});
-				
+    */
+    listaItems.forEach(function(item) {
+      item.addEventListener("click", function() {
+        titulo.innerHTML = campoTexto;
+      });
+      basurita.addEventListener("click", function() {
+        item.style.display = "none";
+        titulo.style.display = "none";
+      });
     });
+  });
 })();
-
-
-
